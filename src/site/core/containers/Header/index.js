473f16d3bdffdb1e0476ad46ebdf7c326b8daec9
Mirren King-Smith <mirren@live.com.au>
2017-10-03 22:38:25 +1000

    Split header, aligned flex positions, cleaned up css

--- src/site/core/containers/Header/index.js
+++ src/site/core/containers/Header/index.js
@@ -2,19 +2,40 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Immutable from 'immutable';
+import { Link, IndexLink } from 'react-router';
 import classNames from 'classnames';
 import { t } from 'i18next';
 
 import styles from './style.less';
+
+import Logo from '../../../../img/header-logo.png';
 
 class Header extends Component {
   constructor(props, context) {
     super(props, context);
   }
 
+  get logo() {
+    return (
+        <IndexLink to="/" >
+          <img className="header-logo-img panel" src={Logo}/>
+        </IndexLink>
+    );
+  }
+
+  get content() {
+    return (
+      <div className="header-content panel">
+        Header
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className={styles.root}>
+        {this.logo}
+        {this.content}
       </div>
     );
   }
