f014393a9be8bed4cb8d6655146da19183a994fe
Mirren King-Smith <mirren@live.com.au>
2017-10-09 11:15:13 +1000

    Added market list header, styling and initial logic

--- src/site/core/containers/Markets/index.js
+++ src/site/core/containers/Markets/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Immutable from 'immutable';
+import classNames from 'classnames';
 import { connect } from 'react-redux';
 import { t } from 'i18next';
 
@@ -9,6 +10,12 @@ import styles from './style.less';
 export class Markets extends Component {
   constructor(props, context) {
     super(props, context);
+
+    this.state = {
+      activeTab: 'time'
+    };
+
+    this.handleTabClick = this.handleTabClick.bind(this);
   }
 
   componentWillMount() {
@@ -19,10 +26,33 @@ export class Markets extends Component {
 
   }
 
+
+
+  get tabs() {
+    return (
+      <div className="market-header">
+        <ul className="tabs">
+          <li className={this.tabClass('time')}><a className="tab" onClick={this.handleTabClick('time')}>Starting Soon</a></li>
+          <li className={this.tabClass('popular')}><a className="tab" onClick={this.handleTabClick('popular')}>Popular</a></li>
+        </ul>
+      </div>
+    );
+  }
+
+  tabClass(tab) {
+    return classNames(['tab-item', {'is-active': tab == this.state.activeTab}]);
+  }
+
+  handleTabClick(tab) {
+    return () => {
+      this.setState({activeTab: tab});
+    }
+  }
+
   render() {
     return (
       <div className={styles.root}>
-
+        {this.tabs}
       </div>
     );
   }
