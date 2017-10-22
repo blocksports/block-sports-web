b84ad2c880c56fc5e3de52bceac92694160c79d8
Mirren King-Smith <mirren@live.com.au>
2017-10-22 14:08:09 +1000

    Updated footer css

--- src/site/core/containers/ExchangeFooter/index.js
+++ src/site/core/containers/ExchangeFooter/index.js
@@ -33,17 +33,25 @@ export class ExchangeFooter extends Component {
   render() {
     return (
       <div className={classNames([styles.root, this.props.className])}>
-        <CurrencyToggle
-          currency={this.props.activeCurrency}
-          exchangeCurrency={this.props.activeExchangeCurrency}
-          onToggle={this.handleToggleClick}
-          />
-        <ViewSlider
-          currency={this.props.activeCurrency}
-          exchangeCurrency={this.props.activeExchangeCurrency}
-          exchangeRate={this.props.exchangeRate}
-          onChange={this.props.updateMinimumBet}
-          />
+        <div className="footer-content block-info">
+
+        </div>
+        <div className="footer-content currency-toggle">
+          <CurrencyToggle
+            currency={this.props.activeCurrency}
+            exchangeCurrency={this.props.activeExchangeCurrency}
+            onToggle={this.handleToggleClick}
+            />
+        </div>
+        <div className="footer-content bet-view">
+          <div className="bet-view-text">{t('core:footer.bet-view')}</div>
+          <ViewSlider
+            currency={this.props.activeCurrency}
+            exchangeCurrency={this.props.activeExchangeCurrency}
+            exchangeRate={this.props.exchangeRate}
+            onChange={this.props.updateMinimumBet}
+            />
+        </div>
       </div>
     );
   }
