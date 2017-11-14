d428f6b4849948ce2e151210bd383dd633204820
Jordan Overbye <jordan@bigfish.tv>
2017-11-15 09:09:52 +1000

    Created basic modal confirm bet component

---
+++ src/site/core/components/Modal/ModalConfirmBet.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import ModalWrapper from './ModalWrapper';
+
+const ModalConfirmBet = props => {
+  return (
+    <ModalWrapper
+    	{...props}
+     	title="Place a bet"
+     	width={400}
+     	showOk={false}
+    >
+    	efoierjforiefj
+    </ModalWrapper>
+  );
+};
+
+export default ModalConfirmBet;
