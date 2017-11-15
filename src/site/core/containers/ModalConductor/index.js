14d4a8603458a268c2c4348205038b5a5ab2b8e4
Jordan Overbye <jordan@bigfish.tv>
2017-11-15 19:23:59 +1000

    Improved modal conductor

--- src/site/core/containers/ModalConductor/index.js
+++ src/site/core/containers/ModalConductor/index.js
@@ -1,25 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Immutable from 'immutable';
+import { setCurrentModal } from '../../reducers/modal';
 
 import ModalConfirmBet from '../../components/Modal/ModalConfirmBet'
 
-const ModalConductor = ({currentModal}) => {
-	console.log(currentModal)
+const ModalConductor = ({currentModal, setCurrentModal, confirmingBet}) => {
 	switch (currentModal) {
   	case 'confirmBet':
-    	return <ModalConfirmBet />
-    	break;
+    	return <ModalConfirmBet setCurrentModal={setCurrentModal} confirmingBet={confirmingBet} />
     default:
       return null;
   }
 };
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setCurrentModal: (...args) => {
+      return dispatch(setCurrentModal(...args));
+    },
+  };
+};
+
 const mapStateToProps = (state) => {
+	console.log(state);
 	return {
-		'currentModal': state.getIn(['core', 'modal', 'currentModal'], Immutable.Map()),
+		'currentModal': state.getIn(['core', 'modal', 'currentModal']),
+		'confirmingBet': state.getIn(['core', 'bet', 'confirmingBet'], Immutable.Map),
 	};
 };
 
-export default connect(mapStateToProps, null)(ModalConductor);
+export default connect(mapStateToProps, mapDispatchToProps)(ModalConductor);
