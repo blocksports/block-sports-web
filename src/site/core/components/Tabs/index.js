546962eeea1fe6e548d46c2ff0ecddf5e3b5918d
Jordan Overbye <jordanoverbye@gmail.com>
2017-11-19 16:12:06 +1000

    Created ‘Tab’ component

---
+++ src/site/core/components/Tabs/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Immutable from 'immutable';
+import classNames from 'classnames';
+import Button from '../Button';
+import styles from './style.less';
+
+export const Tabs = props =>
+  <div className={classNames([props.className, styles.tabs])}>
+    {props.children}
+  </div>
+
+
+export const Tab = props => 
+  <Button className={classNames(['button-tab button-minimal', props.className, styles.tab])} onClick={() => props.onClick()}>
+    {props.children}
+  </Button>
+
+Tabs.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+Tabs.defaultProps = {
+}
+
+Tab.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+Tab.defaultProps = {
+}
---
+++ src/site/core/components/Tabs/index.spec.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import Immutable from 'immutable';
+// import { shallow, mount } from 'enzyme';
+
+// import Button from './';
+
+const minProps = {
+
+};
+
+describe('<Button />', () => {
+  it('Should', () => {
+    expect(true).toEqual(false);
+  });
+});
