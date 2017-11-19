4f6b5b91e6af312045dda8dd54cf6d42d87a711c
Jordan Overbye <jordanoverbye@gmail.com>
2017-11-19 16:11:52 +1000

    Style updates to search

--- src/site/core/components/Search/index.js
+++ src/site/core/components/Search/index.js
@@ -2,28 +2,43 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Immutable from 'immutable';
 import { t } from 'i18next';
-
 import styles from './style.less';
 
 class Search extends Component {
   constructor(props, context) {
     super(props, context);
+    this.state = {
+      value: ''
+    }
+    this.handleMouseEnter = this.handleMouseEnter.bind(this)
   }
 
-  componentWillMount() {
-
+  handleMouseEnter() {
+    this.setState({
+      value: 'Search is currently disabled'
+    })
   }
 
-  componentWillReceiveProps(nextProps) {
-
+  handleMouseLeave() {
+    this.setState({
+      value: ''
+    })
   }
 
   render() {
     return (
-      <span className={styles.root}>
+      <div 
+        className={styles.root} 
+        onMouseEnter={() => this.handleMouseEnter()} 
+        onMouseLeave={() => this.handleMouseLeave()}
+      >
         <i className="fa fa-search search-icon" aria-hidden="true"/>
-        <input className="search" placeholder={t('core:header.search-placeholder')}/>
-      </span>
+        <input 
+          className="search" 
+          placeholder={t('core:header.search-placeholder')} 
+          value={this.state.value}
+        />
+      </div>
     );
   }
 }
