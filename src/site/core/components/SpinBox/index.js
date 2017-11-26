a4b937484f8d5d21f289f56f29c3dea9267d21d5
Jordan Overbye <jordanoverbye@gmail.com>
2017-11-26 19:18:50 +1000

    Spinbox updates

--- src/site/core/components/SpinBox/index.js
+++ src/site/core/components/SpinBox/index.js
@@ -3,44 +3,66 @@ import PropTypes from 'prop-types';
 import Immutable from 'immutable';
 import classNames from 'classnames';
 import { t } from 'i18next';
-
 import styles from './style.less';
+import Button from '../Button';
 
 class SpinBox extends Component {
-  constructor(props, context) {
-    super(props, context);
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: props.value,
+    }
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
   }
 
-  componentWillMount() {
-
-  }
-
-  componentWillReceiveProps(nextProps) {
-
+  componentWillUpdate(nextProps) {
+    if(this.state.inputValue !== nextProps.value) {
+      this.setState({
+        inputValue: nextProps.value
+      })
+    }
   }
 
   handleInputChange(e) {
-    if (e.target.value > parseFloat(this.props.maximum)) return;
+    if (/^[0-9.]*$/.test(e.target.value)) {
+      this.props.onChange(e.target.value);
+    }
+  }
 
-    this.props.onChange(e.target.value);
+  increment() {
+    const nextValue = parseFloat(this.state.inputValue) + 2
+    if (nextValue >= 0) {
+      this.props.onChange(nextValue);
+      this.setState({
+        inputValue: nextValue
+      })
+    }
+  }
+
+  decrement() {
+    const nextValue = parseFloat(this.state.inputValue) - 2
+    if (nextValue >= 0) {
+      this.props.onChange(nextValue);
+      this.setState({
+        inputValue: parseFloat(this.state.inputValue) - 2
+      })
+    }
   }
 
   render() {
     return (
       <div className={classNames([styles.root, this.props.className])}>
         <input
-          value={this.props.value}
+          className={styles.input}
+          value={this.state.inputValue}
           placeholder={this.props.placeholder}
-          onChange={this.handleInputChange}
-          type="number"
-          min={this.props.minimum}
-          max={this.props.maximum}
-          />
-
-        <div>
-
+          onChange={(e) => this.handleInputChange(e)}
+        />
+        <div className={styles.buttons}>
+          <Button className={styles.increment} onClick={() => this.increment()} />
+          <Button className={styles.decrement} onClick={() => this.decrement()} />
         </div>
       </div>
     );
@@ -50,14 +72,7 @@ class SpinBox extends Component {
 SpinBox.propTypes = {
   value: PropTypes.oneOfType([ PropTypes.number, PropTypes.string ]).isRequired,
   onChange: PropTypes.func.isRequired,
-  placeholder: PropTypes.string,
   className: PropTypes.string,
-  minimum: PropTypes.number,
-  maximum: PropTypes.number
 };
-
-SpinBox.defaultProps = {
-  minimum: 0
-}
 
 export default SpinBox;
