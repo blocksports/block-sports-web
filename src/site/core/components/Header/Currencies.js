5367af5a5f2c8526f262222f5de25d569fba6ecb
Jordan Overbye <jordanoverbye@gmail.com>
2017-12-09 15:34:28 +1000

    Added a fade in to header currencies after they are loaded

--- src/site/core/components/Header/Currencies.js
+++ src/site/core/components/Header/Currencies.js
@@ -1,38 +1,62 @@
 import React from 'react';
+import Transition from 'react-transition-group/Transition';
 import PropTypes from 'prop-types';
 import { t } from 'i18next';
 import classNames from 'classnames';
 import styles from './styles.less';
 import SkeletonBlock from '../SkeletonBlock';
 
-const Currencies = (props) => (
-  <div className={styles.currencies}>
-    <Currency {...props} currency="NEO" />
-    <Currency {...props} currency="GAS" />
-  </div>
-)
+const duration = 400;
 
-const Currency = ({ isLoadingCurrency, currency, price, exchangeCurrency }) => {
-  const isLoading = isLoadingCurrency || !price.getIn([currency, exchangeCurrency])
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered:  { opacity: 1 },
+};
+
+const Currencies = (props) => {
+  const isLoading = props.isLoadingCurrency || !props.price.getIn([props.currency, props.exchangeCurrency])
   return (
-    <div>
-      {isLoading &&
+  <div className={styles.currencies}>
+    {isLoading &&
+      <div>
         <div className={styles.currencyLoading}>
           <SkeletonBlock size="large" />
         </div>
-      }
-      {!isLoading &&
-        <div className={styles.currency}>
-          <span className={styles.currencyName}>{t(`core:currency.${currency}`)}</span>
-          <span className={styles.currencyAmount}>
-            {price.getIn([currency, exchangeCurrency]).toFixed(2)}
-            {' '}
-            <span className={styles.exchangeCurrency}>{t(`core:currency.${exchangeCurrency}`)}</span>
-          </span>
+        <div className={styles.currencyLoading}>
+          <SkeletonBlock size="large" />
         </div>
-      }
-    </div>
-  )
-}
+      </div>
+    }
+    {!isLoading &&
+      <Transition appear={true} in={true} timeout={0}>
+        {(state) => (
+          <div style={{
+            ...defaultStyle,
+            ...transitionStyles[state]
+          }}>
+            <Currency {...props} currency="NEO" />
+            <Currency {...props} currency="GAS" />
+          </div>
+        )}
+      </Transition>
+    }
+  </div>
+)}
+
+const Currency = ({ currency, price, exchangeCurrency }) => (
+  <div className={styles.currency}>
+    <span className={styles.currencyName}>{t(`core:currency.${currency}`)}</span>
+    <span className={styles.currencyAmount}>
+      {price.getIn([currency, exchangeCurrency]).toFixed(2)}
+      {' '}
+      <span className={styles.exchangeCurrency}>{t(`core:currency.${exchangeCurrency}`)}</span>
+    </span>
+  </div>
+)
 
 export default Currencies;
