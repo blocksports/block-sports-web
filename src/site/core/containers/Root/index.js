e845deb9df4bc3e1454ffad9644a4e12f77ab01c
Jordan Overbye <jordanoverbye@gmail.com>
2017-12-27 10:43:32 +1000

    Converted root component to stateless component

--- src/site/core/containers/Root/index.js
+++ src/site/core/containers/Root/index.js
@@ -2,28 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Immutable from 'immutable';
-
 import ModalConductor from '../ModalConductor';
 import Layout from '../../components/Layout';
-
 import { fetchUser } from '../../reducers/user';
-
 import styles from './style.less';
 
-class Root extends Component {
-	constructor(props, context) {
-		super(props, context);
-	}
-
-	render() {
-		return (
-			<div className={styles.root}>
-				<Layout>{this.props.children}</Layout>
-				<ModalConductor />
-			</div>
-		);
-	}
-}
+const Root = props => (
+	<div className={styles.root}>
+		<Layout>{props.children}</Layout>
+		<ModalConductor />
+	</div>
+);
 
 Root.propTypes = {
 	user: PropTypes.instanceOf(Immutable.Map).isRequired,
