ae037fbce2ea5fc7b3ee12bc02a2a0cf2b237903
Jordan Overbye <jordanoverbye@gmail.com>
2017-12-29 11:04:52 +1000

    Updated nav list item

--- src/site/core/components/NavList/index.js
+++ src/site/core/components/NavList/index.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Transition from 'react-transition-group/Transition';
-import Immutable from 'immutable';
-import { t } from 'i18next';
-import classNames from 'classnames';
-import Glyph from '../Glyph';
-import { Link, IndexLink } from 'react-router';
-import { categoryFilters } from '../../../../lib/constants';
-import NavListItem from './item';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Transition from "react-transition-group/Transition";
+import Immutable from "immutable";
+import { t } from "i18next";
+import classNames from "classnames";
+import Glyph from "../Glyph";
+import { Link, IndexLink } from "react-router";
+import { categoryFilters } from "../../../../lib/constants";
+import NavListItem from "./item";
 import {
 	fadeDefaultStyle,
-	fadeTransitionStyles,
-} from '../../../../lib/animation';
-import styles from './style.less';
+	fadeTransitionStyles
+} from "../../../../lib/animation";
+import styles from "./style.less";
 
 class NavList extends Component {
 	get category() {
-		return this.props.items.find(item => item.get('id') == this.props.filter);
+		return this.props.items.find(item => item.get("id") == this.props.filter);
 	}
 
 	get header() {
@@ -24,11 +24,11 @@ class NavList extends Component {
 			return <div className={styles.header}>All Markets</div>;
 		}
 		const category = this.category;
-		const name = category ? category.get('name') : '';
+		const name = category ? category.get("name") : "";
 		return (
 			<div className={styles.header}>
 				<span className={styles.headerLink}>
-					<Link to="/exchange">{t('core:navigation.header-all')}</Link>
+					<Link to="/exchange">{t("core:navigation.header-all")}</Link>
 				</span>
 				<Glyph size="14" icon="right" className={styles.headerIcon} />
 				<span className={styles.headerCategory}>{name}</span>
@@ -38,10 +38,15 @@ class NavList extends Component {
 
 	get renderItems() {
 		const category = this.category;
-		const items = category ? category.get('competitions') : this.props.items;
+		const items = category ? category.get("competitions") : this.props.items;
 		if (!items) return null;
 		return items.map((item, i) => (
-			<NavListItem item={item} key={i} category={category} />
+			<NavListItem
+				key={item.get("name")}
+				item={item}
+				category={category}
+				filter={this.props.filter}
+			/>
 		));
 	}
 
@@ -53,8 +58,9 @@ class NavList extends Component {
 						className={styles.root}
 						style={{
 							...fadeDefaultStyle,
-							...fadeTransitionStyles[state],
-						}}>
+							...fadeTransitionStyles[state]
+						}}
+					>
 						{this.header}
 						<nav className={styles.nav}>
 							<ul>{this.renderItems}</ul>
@@ -68,7 +74,7 @@ class NavList extends Component {
 
 NavList.propTypes = {
 	items: PropTypes.instanceOf(Immutable.List).isRequired,
-	filter: PropTypes.string,
+	filter: PropTypes.string
 };
 
 export default NavList;
