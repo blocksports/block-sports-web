cfdd9805c6708789331f333e53734367f14ce2e0
Jordan Overbye <jordanoverbye@gmail.com>
2018-01-01 12:18:47 +1000

    Mobile warning message

--- src/site/core/components/Modal/ModalMobileWarning.js
+++ src/site/core/components/Modal/ModalMobileWarning.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Immutable from 'immutable';
-import ModalWrapper from './ModalWrapper';
-import Glyph from '../Glyph';
-import classNames from 'classnames';
-import styles from './mobileWarning.less';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { t } from "i18next";
+import ModalWrapper from "./ModalWrapper";
+import Glyph from "../Glyph";
+import classNames from "classnames";
+import styles from "./mobileWarning.less";
 
 const ModalMobileWarning = props => (
 	<ModalWrapper {...this.props} allowClose={false}>
 		<div className={styles.root}>
 			<Glyph size="40" icon="mobile" className={styles.icon} />
-			<span className={styles.head}>Sorry! Your screen is too small.</span>
-			<p className={styles.body}>
-				The Block Sports Exchange is not currently available for mobile devices.
-				Upon full release, the Block Sports Exchange will be available for
-				download on iOS and Android.
-			</p>
+			<span className={styles.head}>{t("core:mobile-modal.heading")}</span>
+			<p className={styles.body}>{t("core:mobile-modal.message")}</p>
 		</div>
 	</ModalWrapper>
 );
 
 ModalMobileWarning.propTypes = {
-	setCurrentModal: PropTypes.func.isRequired,
+	setCurrentModal: PropTypes.func.isRequired
 };
 
 export default ModalMobileWarning;
