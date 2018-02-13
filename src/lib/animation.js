df11926bc1bc9d1dd0a44b50b28c9e2e2280ff5a
Jordan Overbye <jordanoverbye@gmail.com>
2018-02-14 09:42:00 +1000

    SlideInUp/Down animations on modals

--- src/lib/animation.js
+++ src/lib/animation.js
@@ -1,7 +1,7 @@
-const fadeDuration = 250;
+export const fadeDuration = 300;
 
 export const fadeDefaultStyle = {
-	transition: `opacity ${fadeDuration}ms ease-in-out`,
+	transition: `all ${fadeDuration}ms ease-in-out`,
 	opacity: 0,
 	visibility: 'hidden',
 };
@@ -9,4 +9,22 @@ export const fadeDefaultStyle = {
 export const fadeTransitionStyles = {
 	entering: { opacity: 0, visibility: 'hidden' },
 	entered: { opacity: 1, visibility: 'visible' },
+};
+
+export const slideUpDuration = 300;
+
+export const slideUpDefaultStyle = {
+	transition: `all ${slideUpDuration}ms ease-in-out`,
+	opacity: 0,
+	visibility: 'hidden',
+	transform: 'translateY(40px)',
+};
+
+export const slideUpTransitionStyles = {
+	entering: {
+		opacity: 0,
+		visibility: 'hidden',
+		transform: 'translateY(40px)',
+	},
+	entered: { opacity: 1, visibility: 'visible', transform: 'translateY(0)' },
 };
--- src/site/core/components/Modal/ModalWrapper.js
+++ src/site/core/components/Modal/ModalWrapper.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Transition from 'react-transition-group/Transition';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
@@ -8,45 +8,102 @@ import styles from './style.less';
 import {
 	fadeDefaultStyle,
 	fadeTransitionStyles,
+	fadeDuration,
+	slideUpDefaultStyle,
+	slideUpTransitionStyles,
+	slideUpDuration,
 } from '../../../../lib/animation';
 
-const ModalWrapper = props => {
-	const handleBackgroundClick = e => {
-		if (props.allowClose) {
-			e.target === e.currentTarget ? props.setCurrentModal(null) : null;
+class ModalWrapper extends Component {
+	constructor() {
+		super();
+		this.state = {
+			appear: true,
+			appearInner: true,
+		};
+		this.handleBackgroundClick = this.handleBackgroundClick.bind(this);
+	}
+
+	handleBackgroundClick(e) {
+		if (this.props.allowClose) {
+			if (e.target === e.currentTarget) {
+				this.setState(
+					{
+						appearInner: false,
+					},
+					() => {
+						setTimeout(() => {
+							this.setState(
+								{
+									appear: false,
+								},
+								() => {
+									setTimeout(() => {
+										this.props.setCurrentModal(null);
+									}, fadeDuration);
+								}
+							);
+						}, fadeDuration / 2);
+					}
+				);
+			}
 		}
-	};
-	return (
-		<Transition appear={true} in={true} timeout={0}>
-			{state => (
-				<div
-					className={styles.root}
-					onClick={handleBackgroundClick}
-					style={{
-						...fadeDefaultStyle,
-						...fadeTransitionStyles[state],
-					}}
-				>
-					<article className={styles.content}>
-						<header>
-							{props.title && <h3 className={styles.heading}>{props.title}</h3>}
-							{props.allowClose && (
-								<Button
-									extras={['square', 'minimal']}
-									className={styles.closeButton}
-									onClick={() => props.setCurrentModal(null)}
+	}
+
+	render() {
+		return (
+			<Transition appear={true} in={this.state.appear} timeout={fadeDuration}>
+				{state => (
+					<div
+						className={styles.root}
+						onClick={this.handleBackgroundClick}
+						style={{
+							...fadeDefaultStyle,
+							...fadeTransitionStyles[state],
+						}}
+					>
+						<Transition
+							appear={true}
+							in={this.state.appearInner}
+							timeout={{
+								enter: fadeDuration * 1.5,
+								exit: slideUpDuration,
+							}}
+						>
+							{innerState => (
+								<article
+									className={styles.content}
+									style={{
+										...slideUpDefaultStyle,
+										...slideUpTransitionStyles[innerState],
+									}}
 								>
-									<Glyph size="14" icon="close" />
-								</Button>
+									<header>
+										{this.props.title && (
+											<h3 className={styles.heading}>{this.props.title}</h3>
+										)}
+										{this.props.allowClose && (
+											<Button
+												extras={['square', 'minimal']}
+												className={styles.closeButton}
+												onClick={() => this.props.setCurrentModal(null)}
+											>
+												<Glyph size="14" icon="close" />
+											</Button>
+										)}
+									</header>
+									<section className={styles.body}>
+										{this.props.children}
+									</section>
+								</article>
 							)}
-						</header>
-						<section className={styles.body}>{props.children}</section>
-					</article>
-				</div>
-			)}
-		</Transition>
-	);
-};
+						</Transition>
+					</div>
+				)}
+			</Transition>
+		);
+	}
+}
 
 ModalWrapper.propTypes = {
 	title: PropTypes.string,
