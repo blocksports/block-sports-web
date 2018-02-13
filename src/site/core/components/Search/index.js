9d2edd8c0f5eb960d4fa3f8b54579eb7676e782b
Jordan Overbye <jordanoverbye@gmail.com>
2018-02-13 17:00:56 +1000

    Search style updates

--- src/site/core/components/Search/index.js
+++ src/site/core/components/Search/index.js
@@ -3,6 +3,7 @@ import Transition from 'react-transition-group/Transition';
 import classNames from 'classnames';
 import { t } from 'i18next';
 import Glyph from '../Glyph';
+import Button from '../Button';
 import dropdown from '../Dropdown';
 import dropdownStyles from '../Dropdown/styles.less';
 import {
@@ -13,12 +14,10 @@ import styles from './style.less';
 
 const Search = props => (
 	<div className={styles.root}>
-		<Glyph size="14" icon="search" className={styles.icon} />
-		<input
-			className={styles.input}
-			placeholder={t('core:header.search-placeholder')}
-			onFocus={() => props.openDropdown()}
-		/>
+		<input className={styles.input} onFocus={() => props.openDropdown()} />
+		<Button className={styles.button}>
+			<Glyph size="14" icon="search" className={styles.icon} />
+		</Button>
 		<Transition in={props.showDropdown} timeout={0}>
 			{state => (
 				<div
@@ -26,12 +25,14 @@ const Search = props => (
 					style={{
 						...fadeDefaultStyle,
 						...fadeTransitionStyles[state],
-					}}>
+					}}
+				>
 					<div
 						className={classNames([
 							dropdownStyles.dropdownInner,
 							styles.dropdownInner,
-						])}>
+						])}
+					>
 						<div className={dropdownStyles.dropdownWarning} />
 						<Glyph size="40" icon="search" className={styles.iconLarge} />
 						<span>{t('core:header.search-disabled-message')}</span>
